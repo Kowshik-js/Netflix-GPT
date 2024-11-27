1571061f9c98f50dcdcfb6f62c0e671e97eaa749
Kowshik-js <142918527+Kowshik-js@users.noreply.github.com>
2024-11-27 23:56:41 +0530

    tailer fetched

--- src/components/videoBackground.js
+++ src/components/videoBackground.js
@@ -1,8 +1,44 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrailerVideo } from "../utils/moviesSlice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-const VideoBackground = () => {
+const VideoBackground = ({movieId}) => {
+  const trailerVideo = useSelector(store=> store.movies?.trailerVideo);
+  const dispatch = useDispatch();
+  
+  
+
+const getMovieVideos = async () =>{
+  const data=await fetch
+  ('https://api.themoviedb.org/3/movie/1100782/videos?language=en-US',
+     API_OPTIONS);
+  const json = await data.json();
+  console.log(json);
+
+  const filterData = json.results.filter(video => video.type === "Trailer");
+  const trailer = filterData.length ? filterData[0] : json.results[0];
+  console.log(trailer);
+  dispatch(addTrailerVideo(trailer));
+  };
+  useEffect(()=>{
+    getMovieVideos();
+  },[]
+)
+
+
   return (
-    <div>videoBackground</div>
+    <div><iframe 
+    src={"https://www.youtube.com/embed/FU_bAopCcSE?si=OXDj5LA1drIhAdHc" +trailerVideo.key}
+    title="YouTube video player" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    referrerpolicy="strict-origin-when-cross-origin" 
+    >
+    </iframe></div>
   )
 }
 
 export default VideoBackground;
+
+

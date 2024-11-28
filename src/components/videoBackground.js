59a90dfb2067c1ef9fb7e69f3c7d33e77b447089
Kowshik-js <142918527+Kowshik-js@users.noreply.github.com>
2024-11-28 23:42:43 +0530

    auto play of trailer and mainCountainer has been done

--- src/components/videoBackground.js
+++ src/components/videoBackground.js
@@ -1,44 +1,57 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/moviesSlice";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-const VideoBackground = ({movieId}) => {
-  const trailerVideo = useSelector(store=> store.movies?.trailerVideo);
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   const dispatch = useDispatch();
-  
-  
 
-const getMovieVideos = async () =>{
-  const data=await fetch
-  ('https://api.themoviedb.org/3/movie/1100782/videos?language=en-US',
-     API_OPTIONS);
-  const json = await data.json();
-  console.log(json);
+  const getMovieVideos = async () => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+        API_OPTIONS
+      );
 
-  const filterData = json.results.filter(video => video.type === "Trailer");
-  const trailer = filterData.length ? filterData[0] : json.results[0];
-  console.log(trailer);
-  dispatch(addTrailerVideo(trailer));
+      if (!response.ok) {
+        throw new Error("Failed to fetch trailer videos");
+      }
+
+      const json = await response.json();
+      const filterData = json.results.filter((video) => video.type === "Trailer");
+      const trailer = filterData.length ? filterData[0] : json.results[0];
+
+      dispatch(addTrailerVideo(trailer));
+    } catch (error) {
+      console.error("Error fetching movie videos:", error);
+    }
   };
-  useEffect(()=>{
-    getMovieVideos();
-  },[]
-)
 
+  useEffect(() => {
+    if (movieId) {
+      getMovieVideos();
+    }
+  }, [movieId]);
 
   return (
-    <div><iframe 
-    src={"https://www.youtube.com/embed/FU_bAopCcSE?si=OXDj5LA1drIhAdHc" +trailerVideo.key}
-    title="YouTube video player" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-    referrerpolicy="strict-origin-when-cross-origin" 
-    >
-    </iframe></div>
-  )
-}
+    <div className="w-screen ">
+      {trailerVideo ? (
+        <iframe
+        className="w-screen aspect-video"
+          src={`https://www.youtube.com/embed/${trailerVideo.key}?autoplay=1&mute=1`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          width="100%"
+          height="100%"
+          allowFullScreen
+        ></iframe>
+      ) : (
+        <p>Loading trailer...</p>
+      )}
+    </div>
+  );
+};
 
 export default VideoBackground;
-
-
--- src/components/videoTitle.js
+++ src/components/videoTitle.js
@@ -1,15 +1,21 @@
 const VideoTitle = ({ title, overview }) => {
-    return (
-      <div className="pt-36 px-12">
-        <h1 className="text-6xl font-bold">{title}</h1>
-        <p className="py-6 text-lg w-1/4">{overview}</p>
-        <div className="">
-          <button className="bg-gray-500 text-white p-4 px-16 text-xl bg-opacity-50 rounded-lg">Play</button>
-          <button className="ms-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">More Info</button>
-        </div>
+  return (
+    <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
+      <h1 className="text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <div className="flex gap-4">
+        {/* Play Button */}
+        <button className="bg-white text-black font-semibold px-8 py-3 rounded-md hover:bg-gray-200 transition-all duration-300">
+          ▶️ Play
+        </button>
+
+        {/* More Info Button */}
+        <button className="bg-gray-500 bg-opacity-70 text-white font-semibold px-8 py-3 rounded-md hover:bg-gray-400 transition-all duration-300">
+          ℹ️ More Info
+        </button>
       </div>
-    );
-  };
-  
-  export default VideoTitle;
-  
+    </div>
+  );
+};
+
+export default VideoTitle;
